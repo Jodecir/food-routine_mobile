0fa0e8304af635099b2dba85722860ae21f425c7
Jodecir Neto <jodecirneto@gmail.com>
2022-02-28 17:31:24 -0300

    UPDATE intro page with slide text vars

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -12,4 +12,21 @@ export class IntroPage implements OnInit {
   ngOnInit() {
   }
 
+  slides = [
+    {
+      title: "Welcome",
+      description: "Bem vindo ao <b>Food-Routine</b>, você pode montar sua rotina alimentar com o auxílio de um profissional sem perder a flexibilidade.",
+      image: "assets/img/slidebox-img-1.png",
+    },
+    {
+      title: "Simple",
+      description: "Tudo de forma <b>simples</b> e <b>confortável</b>, alguns cliques e já está feito sua rotina alimentar ideal.",
+      image: "assets/img/slidebox-img-2.png",
+    },
+    {
+      title: "Flex",
+      description: "Nunca foi tão fácil ser saúdavel de maneira flexível, então oquê você está esperando agora ?",
+      image: "assets/img/slidebox-img-3.png",
+    }
+  ];
 }
