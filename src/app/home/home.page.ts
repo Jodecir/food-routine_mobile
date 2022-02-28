f1cb88fe10d0c8f2b443d17cd63f51b15194265b
Jodecir Neto <jodecirneto@gmail.com>
2022-02-28 15:22:31 -0300

    CREATE nutritionist alert var

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
   public nome_nutricionista:string = "Nutricionista"
+  public aviso_nutricionista:string = "Não deixe de comer as frutas, legumes e verduras."
+  
   constructor() { }
 
   ngOnInit() {
