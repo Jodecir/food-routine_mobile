7de2e08c75fc003e544ab82d035aa2f0850351fc
Jodecir Neto <jodecirneto@gmail.com>
2022-03-01 16:32:06 -0300

    CREATE logic in food-list

--- src/app/pages/Food/food-list/food-list.page.ts
+++ src/app/pages/Food/food-list/food-list.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LoadingController, ToastController } from'@ionic/angular';
+
+import { FoodService } from'src/app/services/food.service';
+import { Food } from'src/app/interfaces/food';
+
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.page.html',
@@ -7,9 +14,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FoodListPage implements OnInit {
 
-  constructor() { }
+  public foodList = new Array<Food>();
+  private loading:any;
+  private produtoSubscription:Subscription;
+  
+  constructor(
+    private loadingCtrl:LoadingController,
+    private produtoService:FoodService,
+    private toastCtrl:ToastController
+  ) {
+    this.produtoSubscription = this.produtoService.listaFood().subscribe(data=>{
+      this.foodList = data;
+    });
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.produtoSubscription.unsubscribe();
+  }
+  
+  async presentLoading() { 
+    this.loading = await this.loadingCtrl.create({message:'Aguarde...'});
+    return this.loading.present();
+  }
+
+  async deleteFood(id:string) { 
+    try { await this.produtoService.excluirFood(id);
+    } catch(error) {
+      this.presentToast('Erro ao tentar deletar');
+    }
+  }
+
+  async presentToast(message:string) {
+    const toast = await this.toastCtrl.create({message,duration:2000});
+    toast.present();
+  }
 }
