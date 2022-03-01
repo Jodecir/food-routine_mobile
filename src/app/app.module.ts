b31353c08cb9111fffbb595da8b15122a4bf111e
Jodecir Neto <jodecirneto@gmail.com>
2022-03-01 12:39:02 -0300

    CREATE firebase config for project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
+import { AngularFireModule } from"@angular/fire/compat";
+import { AngularFireAuthModule } from"@angular/fire/compat/auth";
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+import { environment } from"../environments/environment";
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBJHz9dgirpakauUQr_Ja-SvEF72hp6_g0",
+    authDomain: "food-routine.firebaseapp.com",
+    projectId: "food-routine",
+    storageBucket: "food-routine.appspot.com",
+    messagingSenderId: "521481123838",
+    appId: "1:521481123838:web:27f036d73042194884b2af",
+    measurementId: "G-J00S0KCST9"
+  }
 };
