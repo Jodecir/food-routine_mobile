9c54886ffcdc2e2d99fc5f6c430423ea63593601
Jodecir Neto <jodecirneto@gmail.com>
2022-03-01 16:34:47 -0300

    CREATE fake logoff in navbar home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LoadingController } from'@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+
   public nome_nutricionista:string = "Nutricionista"
   public aviso_nutricionista:string = "Não deixe de comer as frutas, legumes e verduras."
-  
-  constructor() { }
+  private loading:any;
+
+  constructor(
+    private loadingCtrl:LoadingController,
+  ) { }
 
   ngOnInit() {
+  }
+
+  async presentLoading() { 
+    this.loading = await this.loadingCtrl.create({message:'Aguarde...'});
+    return this.loading.present();
+  }
+
+  async logout() { 
+    await this.presentLoading();
   }
 
 }
