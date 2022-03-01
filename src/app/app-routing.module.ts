dfc94c9baa696f199f1773742f77b1861953b246
Jodecir Neto <jodecirneto@gmail.com>
2022-03-01 19:36:32 -0300

    CREATE registration page forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
     path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
@@ -13,10 +18,6 @@ const routes: Routes = [
   {
     path: 'about',
     loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
-  },
-  {
-    path: 'user-registration',
-    loadChildren: () => import('./pages/User/user-registration/user-registration.module').then( m => m.UserRegistrationPageModule)
   },
   {
     path: 'intro',
@@ -31,12 +32,13 @@ const routes: Routes = [
     loadChildren: () => import('./pages/Food/food-list/food-list.module').then( m => m.FoodListPageModule)
   },
   {
-    path: '',
+    path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'registration',
+    loadChildren: () => import('./registration/registration.module').then( m => m.RegistrationPageModule)
   }
-
-
-
 ];
 @NgModule({
   imports: [
--- src/app/registration/registration-routing.module.ts
+++ src/app/registration/registration-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { UserRegistrationPage } from './user-registration.page';
+import { RegistrationPage } from './registration.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: UserRegistrationPage
+    component: RegistrationPage
   }
 ];
 
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class UserRegistrationPageRoutingModule {}
+export class RegistrationPageRoutingModule {}
