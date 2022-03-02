3f24e03a389926bc8f0c3d8ad9f75dc0e3eddc88
Jodecir Neto <jodecirneto@gmail.com>
2022-03-02 14:20:03 -0300

    CREATE logic in login page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from"@angular/fire/compat/auth";
+
+import { LoadingController, NavController, ToastController , Platform } from"@ionic/angular";
+
+import { User } from'../models/user.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  user = {} as User;
+  verificar: any;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private loadingCtrl: LoadingController,
+    private navCtrl: NavController,
+    private toastCtrl: ToastController,
+    private platform: Platform
+  ) {}
 
   ngOnInit() {
+  }
+
+  ionViewWillLeave() { 
+    this.verificar.unsubscribe(); 
+  }
+
+  ionViewDidEnter() { 
+    this.verificar = this.platform.backButton.subscribe(() => { navigator["app"].exitApp(); }); 
+  }
+
+  async login(user: User) {
+    if (this.formValidation()) {
+      let loader = await this.loadingCtrl.create({ message: "Por Favor Espere..." }); loader.present();
+      try {
+        await this.afAuth .signInWithEmailAndPassword(user.email, user.password) .then(data => { console.log(data); this.navCtrl.navigateRoot("home"); }) .catch(); 
+      } 
+      catch (e) { this.showToast(e); } loader.dismiss();
+    }
+  }
+
+  formValidation() { 
+    if (!this.user.email) { 
+      this.showToast("Digite seu e-mail"); return false; 
+    }
+    if (!this.user.password) { 
+      this.showToast("Digite sua senha"); return false; 
+    }
+    return true;
+  }
+  
+  showToast(mensagem: string) { 
+    this.toastCtrl .create({ message: mensagem, duration: 3000 }).then(toastData => toastData.present()); 
   }
 
 }
