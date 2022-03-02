abfe4c7b86ffcb45c9dee6ae1089d39dde9657bb
Jodecir Neto <jodecirneto@gmail.com>
2022-03-02 14:21:13 -0300

    CREATE logic in registration page

--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from"@angular/fire/compat/auth";
+
+import { User } from'../models/user.model';
+
+import { ToastController, LoadingController, NavController } from"@ionic/angular";
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +12,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationPage implements OnInit {
 
-  constructor() { }
+  user = {} as User;
+
+  constructor(
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private navCtrl: NavController,
+    private afAuth: AngularFireAuth
+  ) {}
 
   ngOnInit() {
+  }
+
+  async register(user: User) {
+    console.log(user);
+    
+    if(this.formValidation()) {
+      // mostrar loader
+      let loader = await this.loadingCtrl.create({
+        message: "Por Favor espere..."
+      });
+      loader.present();
+
+      try {
+        // entrar com email e password
+        await this.afAuth.
+        createUserWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+          console.log(data);
+          // redireciona para outra página
+          this.navCtrl.navigateRoot("login");
+        })
+        .catch();
+      } 
+      catch (e) {this.showToast(e);}
+      // dispensar loader
+      loader.dismiss();
+    }
+  }
+
+  formValidation() {
+    if(!this.user.email) {
+      // mostrar toast message 
+      this.showToast("Digite seu e-mail");
+      return false;
+    }
+    if(!this.user.password) {
+      // mostrar toast message 
+      this.showToast("Digite sua Senha");
+      return false;
+    }
+    return true;
+  }
+
+  showToast(mensagem: string) {
+    this.toastCtrl.create({message: mensagem,duration: 3000})
+    .then(toastData=> toastData.present());
   }
 
 }
